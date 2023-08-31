0e0bc77ded3e198fd08a1bef621c48d0b2376c95
Mohnish <95386817+MussMan67@users.noreply.github.com>
2023-08-31 15:17:25 -0500

    Update scc.java

--- scc.java
+++ scc.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 import java.util.Arrays;
 public class scc{
 
@@ -10,33 +9,38 @@ public class scc{
 //MAINNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
 
     public static void main(String[] args) {
-        String space = " ";
-/*      Scanner sc = new Scanner(System.in);
+        String space = "-";
+
+/* 
+        Scanner sc = new Scanner(System.in);
         System.out.println("");
-        String l1 = sc.nextLine();
+        String l1 = sc.nextLine().substring(0, 8);
         System.out.println("");
-        String l2 = sc.nextLine();
+        String l2 = sc.nextLine().substring(0, 8);
         System.out.println("");
-        String l3 = sc.nextLine();
+        String l3 = sc.nextLine().substring(0, 8);
         System.out.println("");
-        String l4 = sc.nextLine();
+        String l4 = sc.nextLine().substring(0, 8);
         System.out.println("");
-        String l5 = sc.nextLine();
+        String l5 = sc.nextLine().substring(0, 8);
         System.out.println("");
-        String l6 = sc.nextLine();
+        String l6 = sc.nextLine().substring(0, 8);
         System.out.println("");
-        String l7 = sc.nextLine();
+        String l7 = sc.nextLine().substring(0, 8);
         System.out.println("");
-        String l8 = sc.nextLine();
+        String l8 = sc.nextLine().substring(0, 8);
         System.out.println("");
-        String l9 = sc.nextLine();
+        String l9 = sc.nextLine().substring(0, 8);
         String[] rawStrings = {l1,l2,l3,l4,l5,l6,l7,l8,l9};
         
         int[][] sudoku = {{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},};
 
         for(int j = 0; j<9; j++){
+            String lineString = rawStrings[j];
+            String[] line = lineString.split("");
             for(int i = 0; i<9; i++){
-                String num = rawStrings[j].substring(i,i+1);
+                String num = line[i];
+                
                 if((num.equals(space))){
                     sudoku[j][i] = 0;
                 }
@@ -45,19 +49,43 @@ public class scc{
                 }
             }
         }
-
+*/
         //int num = Integer.parseInt(String.valueOf(let));
-*/        
-    
-        int[][] sudoku = {{1,1,1,1,1,1,1,1,1},{2,2,2,2,2,2,2,2,2},{3,3,3,3,3,3,3,3,3},{4,4,4,4,4,4,4,4,4},{5,5,5,5,5,5,5,5,5},{6,6,6,6,6,6,6,6,6},{7,7,7,7,7,7,7,7,7},{8,8,8,8,8,8,8,8,8},{9,9,9,9,9,9,9,9,9},};
 
-        System.out.println(Arrays.deepToString(sudoku));
+        int[][] sudoku = {{1,8,5,4,0,0,0,6,3},{9,6,0,0,0,0,0,0,5},{3,0,4,0,0,0,0,0,0},{0,0,0,0,8,0,0,0,0},{0,4,0,2,5,0,8,3,0},{8,2,7,0,1,0,0,4,0},{4,9,0,0,3,0,0,0,0},{0,0,0,0,0,0,2,0,0},{0,1,8,0,4,5,0,0,0},};
+
+
         Sudoku s = new Sudoku(sudoku);
+        /*for(int i = 0;i<sudoku.length-1;i++){
+            for(int j =0; j<sudoku[i].length-1;j++){
+                System.out.println(s.findSquare(i,j)+": "+i+", "+j);
+            }
+        }*/
 
-        
+        System.out.println("      1    |    2    |    3    |    4    |    5    |    6    |    7    |    8    |    9");
+        for(int i = 0; i < sudoku.length-1; i++){
+            System.out.print((i+1)+" ");
+            for(int j = 0; j < sudoku[i].length-1;j++){
+                System.out.println("dd");
+                if(sudoku[i][j]==0){
+                    for(int num : s.cellCandidates(i, j)){
+                        if(num!=0){
+                            System.out.print(String.valueOf(num));
+                        }
+                        else if(num==0){
+                            System.out.print(' ');
+                        }
+                    }
+                    System.out.print("|");
+                    
+                }
+                else if(sudoku[i][j]!=0){
+                    System.out.print("----"+sudoku[i][j]+"----|");
+                }
+            }
+            System.out.println("");
+        }
     }
 
 //MAINNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
-
-
 }
